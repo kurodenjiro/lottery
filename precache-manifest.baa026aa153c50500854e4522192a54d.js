e0c3089229ecc118deca51a1882b58e99c81b919
Kurodenjiro <kurodenjiro1@gmail.com>
2021-08-21 18:59:23 +0700

    Updates

---
+++ precache-manifest.baa026aa153c50500854e4522192a54d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1794f7f658d68b1e9830190642fe3906",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "2822f9ab37eaf8b5a2e4",
+    "url": "/lottery/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "867c398c44447979c213",
+    "url": "/lottery/static/js/2.473376ce.chunk.js"
+  },
+  {
+    "revision": "2822f9ab37eaf8b5a2e4",
+    "url": "/lottery/static/js/main.ceff97ad.chunk.js"
+  },
+  {
+    "revision": "701f6ffa1d484f10e2cf",
+    "url": "/lottery/static/js/runtime-main.2ccda5d4.js"
+  }
+]);
